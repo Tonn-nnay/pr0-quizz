804db22ed51435927ee6bdb1aea7decb3bb8773a
Tonn-nnay <tymartinmarin@gmail.com>
2025-09-22 09:17:45 +0200

    initial commit

---
+++ functions/getPreguntes.php
@@ -0,0 +1,52 @@
+<?php
+    session_start();
+    $_SESSION['respostes'] = array();
+
+    if(!isset($_GET['quantitat'])){
+        echo "no s'ha rebut quantitat";
+    } else { //:35
+
+////////////////////////////////////////////////////////////////////////////// Conseguir el json
+
+        $json = file_get_contents("../rsc/json/data.json");
+
+        if ($json === false) {
+            die('No ha cargado del json');
+        } else { //:34
+            $json_data = json_decode($json, true);
+
+////////////////////////////////////////////////////////////////////////////// Creació d'array de respostes i de array preguntes, respostes
+
+        $llistat_ids = array_rand($json_data['preguntes'], $_GET['quantitat']); 
+        $llistat_js = array();
+        $i = 0;
+        
+        foreach ($json_data['preguntes'] as $index => $pregunta) { 
+            if (in_array($index, $llistat_ids)) { 
+                $value_id = $pregunta['id'];
+                $value_pregunta = $pregunta['pregunta'];
+                $value_respostes_total = array();
+
+                foreach ($pregunta['respostes'] as $resposta) { 
+                    $value_respostes = array($resposta['id'], $resposta['resposta']);
+                    $value_respostes_total[] = $value_respostes;
+
+                    if ($resposta['correcta'] === true) {
+                        $_SESSION['respostes'][] = $resposta['id'];
+                    }
+                }
+                $llistat_js[$i]['id'] = $value_id;
+                $llistat_js[$i]['pregunta'] = $value_pregunta;
+                $llistat_js[$i]['respostes'] = $value_respostes_total;
+                $i++;
+            }
+        }
+
+////////////////////////////////////////////////////////////////////////////////
+
+            $return = json_encode($llistat_js);
+            echo $return;
+        }     
+    }
+
+?>
---
+++ js/modulo.js
@@ -0,0 +1,61 @@
+let estatDeLaPartida = { 
+    contadorPreguntes: 0, 
+    respostesUsuari: []
+}; 
+
+function actualitzarMarcador() {
+    let marcador = document.getElementById("marcador");
+    marcador.innerHTML = `<p>Preguntes respostes: ${estatDeLaPartida.contadorPreguntes} de 10</p>`;
+}
+
+function marcarRespuesta(pregunta, resposta) {
+    console.log("Pregunta: " + pregunta + " / Resposta: " + resposta);
+    let num = pregunta - 1;
+    
+    if(estatDeLaPartida.respostesUsuari[num] == undefined){
+        estatDeLaPartida.contadorPreguntes ++;
+    }
+
+    estatDeLaPartida.respostesUsuari[num] = resposta;
+
+    actualitzarMarcador();
+    console.log(estatDeLaPartida.respostesUsuari);
+}
+window.marcarRespuesta = marcarRespuesta;
+
+function renderitzarMarcador(){
+    let contador = estatDeLaPartida.contadorPreguntes;
+    
+    let marcador = document.getElementById("marcador");
+    let htmlString = `<p>Preguntes respostes: ${estatDeLaPartida.contadorPreguntes} de 10</p>`;
+
+    marcador.innerHTML = htmlString;
+}
+
+function renderTaulell(data){
+    console.log(data);
+
+    renderitzarMarcador();
+
+    let partida = document.getElementById("partida");
+    let htmlString="";
+
+    //revisar
+    for (var k in data){
+        if(data.hasOwnProperty(k)){
+            htmlString+=`<h3> ${data[k].pregunta} </h3>`;
+            for (let i = 0; i < data[k].respostes.length; i++){
+                htmlString+=`<button onclick="marcarRespuesta(${data[k].id+1},${i+1})"> ${data[k].respostes[i][1]}</button>`
+            }            
+        }
+    }
+    partida.innerHTML = htmlString;
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+
+    fetch ('http://localhost/pr0/functions/getPreguntes.php?quantitat=10')
+        .then(response => response.json())
+        .then(data => renderTaulell(data));
+    
+} );
