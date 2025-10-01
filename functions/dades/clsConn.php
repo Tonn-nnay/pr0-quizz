a5d5e658f7fc0189f7fb9997b058f2b54c17c158
Tonn-nnay <tymartinmarin@gmail.com>
2025-10-01 09:55:43 +0200

    produccio

--- functions/dades/clsConn.php
+++ functions/dades/clsConn.php
@@ -1,36 +1,38 @@
 <?php
-class clsConn{
-    //hay errores pq VisualStudio no entiende que la variable $conn se define en el constructor
-        public $conn;
+class clsConn {
+    public $conn;
 
-        function __construct($_servername, $_username, $_password, $_db){
-            $this->$conn = mysqli_connect($_servername, $_username, $_password, $_db);
-            if (!$this->$conn) {
-                die("Connection failed: " . mysqli_connect_error());
-            }
-        }
-
-        function free(){
-            $this->$conn->close();
-        }
-
-        public function query_cud($sql){
-            if ($this->$conn->query($sql) === TRUE){
-                return TRUE;
-            } else {
-                return "Error: " . $sql . "<br>" . $conn->error;
-            }
-        }
-
-        public function query_r($sql){
-            $data = $this->$conn->query($sql);
-            if(isset($data)){
-                $result = [];
-                while ($row = $data->fetch_assoc()) {
-                    $result[] = $row;
-                }
-                return $result;
-            }
+    function __construct($_servername, $_username, $_password, $_db) {
+        $this->conn = mysqli_connect($_servername, $_username, $_password, $_db);
+        
+        if (!$this->conn) {
+            die("Connection failed: " . mysqli_connect_error());
         }
     }
+
+    function free() {
+        $this->conn->close();
+    }
+
+    public function query_cud($sql) {
+        if ($this->conn->query($sql) === TRUE) {
+            return TRUE;
+        } else {
+            return "Error: " . $sql . "<br>" . $this->conn->error;
+        }
+    }
+
+    public function query_r($sql) {
+        $data = $this->conn->query($sql);
+        if ($data) {
+            $result = [];
+            while ($row = $data->fetch_assoc()) {
+                $result[] = $row;
+            }
+            return $result;
+        } else {
+            return null;
+        }
+    }
+}
 ?>
