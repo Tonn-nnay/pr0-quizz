c9908d3d79c92d98941472d01ad029f7ada89a0c
Tonn-nnay <tymartinmarin@gmail.com>
2025-10-05 23:01:55 +0200

    començament_crud

--- functions/consulta.php
+++ functions/consulta.php
@@ -5,7 +5,7 @@
     $conn = new clsConn($servername, $username, $password, $db);
 
     switch($_GET['action']){
-            case('insert' || 'delete' || 'update'):
+            case('insert'):
                 $result = $conn->query_cud($sql);
                 break;
             case('delete'):
@@ -16,7 +16,9 @@
                 break;
             default:
                 $sql = "WITH respuestas_limitadas AS (SELECT r.ID_pregunta, r.ID_resposta, r.Resposta, r.Nom, r.Correcte, ROW_NUMBER() OVER (PARTITION BY r.ID_pregunta ORDER BY r.ID_resposta) AS rn FROM Respostes r) SELECT JSON_ARRAYAGG(JSON_OBJECT('ID_pregunta', p.ID_pregunta, 'Pregunta', p.Pregunta, 'Respostes', (SELECT JSON_ARRAYAGG(JSON_OBJECT('ID_resposta', rl2.ID_resposta, 'Resposta', rl2.Resposta, 'Nom', rl2.Nom, 'Correcte', rl2.Correcte)) FROM respuestas_limitadas rl2 WHERE rl2.ID_pregunta = p.ID_pregunta AND rl2.rn <= 4))) AS preguntas_json FROM Preguntes p;";        
-                $json_data = json_decode($conn->query_r($sql), 1);
+                $result = $conn->query_r($sql);
+                $json_bruto = $result[0]['preguntas_json'];
+                $json_data = json_decode($json_bruto, 1);
                 echo $json_data;
                 break;
         }
--- js/admin.js
+++ js/admin.js
@@ -2,24 +2,13 @@
 Index
     AEL ('DOMContentLoaded')
 */
+let logic = 0;
 
 function fetchRender(){
     fetch (`functions/consulta.php?action="read"`)
         .then(response => response.json())
         .then(data => render(data));
 }
-
-/*function fetchRender(query, info){
-    //convertir la info en el json
-    fetch("functions/consulta.php", {
-                    method: "POST",
-                    headers: {"Content-Type": "application/json"},
-                    body: JSON.stringify({
-                        action: query,
-
-                    })
-                })
-}*/
 
 window.addEventListener("DOMContentLoaded", (event) => {
     fetchRender();
@@ -28,5 +17,40 @@ window.addEventListener("DOMContentLoaded", (event) => {
 function render(data){
     let print = document.getElementsByClassName(menu);
     print.innerHTML = "";
-    print.innerHTML = `${data}`;
+    for (let k in data){
+        if(data.hasOwnProperty(k)){
+            htmlString +=  `<div class="pregunta">
+                                <label for="${data[k].ID_pregunta}">Pregunta:</label>
+                                <input type="text" id="${data[k].ID_pregunta}" name="${data[k].pregunta}">${data[k].pregunta}</input>
+                                <button id="eliminar" data-id="${data[k].ID_pregunta}">Eliminar pregunta</button>
+                                <button id="actualitzar">Actualitzar</button>
+                            </div>`
+        }
+    }
+
+    if (logic == 0){
+        print.addEventListener('click', function(e) {
+            switch(e.target.id){
+                case 'eliminar':
+                    fetch('function/consulta.php', {
+                        method: "POST",
+                        headers: {"Content-Type": "application/json"},
+                        body: JSON.stringify({
+                            case: 'delete',
+                            id: e.target.data-id
+                    })
+                }).then()
+            } 
+        })
+        logic ++;
+    } 
+}
+
+function crear(){
+    let print = document.getElementsByClassName(menu);
+    print.innerHTML += `<div class="pregunta">
+                            <label for="new">Pregunta:</label>
+                            <input type="text" id="new" name="new"></input>
+                            <button id="nou">Pujar</button>
+                        </div>`
 }
